8f6e37c016403eda418ada07b92bfcf60420b3c1
pain459 <ravi.ameer@gmail.com>
2024-03-16 11:07:27 +0530

    1. Created requirements.txt and listed the requirements. 2. imported requirements.txt into setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import find_packages, setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='formula_bin',
     packages=find_packages(include=['formulas']),
     version='0.1.0',
     description='Formula implementations in Python',
-    author='Me',
-    install_requires=[],
+    author='Pain',
+    install_requires=requirements,
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     test_suite='tests',
