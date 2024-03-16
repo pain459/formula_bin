c88293ee488bcaf7eeaf6fd3af6cad63d1449394
pain459 <ravi.ameer@gmail.com>
2024-03-16 09:48:28 +0530

    1. Square formulas and tests added.

---
+++ formulas/basic/square.py
@@ -0,0 +1,10 @@
+# Find area of a square
+def area(a):
+    """Calculates area of a square"""
+    return a ** 2
+
+
+# Find perimeter of a square
+def perimeter(a):
+    """Calculate perimeter of a square."""
+    return 4 * a
