d5df0306d3ab3ab995aa98f43550ab8838cdc088
pain459 <ravi.ameer@gmail.com>
2024-03-18 07:48:08 +0530

    New branch and length formulas started.

---
+++ formulas/convert/length.py
@@ -0,0 +1,18 @@
+def inch_to_centimeter(x):
+    """
+    Function to convert from inch to centimeter
+    :param x: input in inches
+    :return: converted to centimeters
+    """
+    return x * 2.540
+
+
+def centimeter_to_inch(x):
+    """
+    Function to convert from centimeter to inch
+    :param x: input in centimeters
+    :return: converted to inches
+    """
+    return x / 2.540
+
+
