369d17fa76c6c9e0a10a652f46e704be76d30d32
pain459 <ravi.ameer@gmail.com>
2024-03-31 18:48:39 +0530

    mass.py updated with formulas.

---
+++ formulas/convert/mass.py
@@ -0,0 +1,88 @@
+def milligram_to_gram(x):
+    """
+    Function to convert milligram to gram
+    :param x: Input in milligram
+    :return: Convert to gram
+    """
+    return x * 0.001
+
+
+def centigram_to_gram(x):
+    """
+    Function to convert centigram to gram
+    :param x: Input in centigram
+    :return: Convert to gram
+    """
+    return x * 0.01
+
+
+def decigram_to_gram(x):
+    """
+    Function to convert decigram to gram
+    :param x: Input in decigram
+    :return: Convert to gram
+    """
+    return x * 0.1
+
+
+def decagram_to_gram(x):
+    """
+    Function to convert decagram to gram
+    :param x: Input in decagram
+    :return: Convert to gram
+    """
+    return x * 10
+
+
+def ounce_to_gram(x):
+    """
+    Function to convert ounce to gram
+    :param x: Input in ounce
+    :return: Convert to gram
+    """
+    return x * 28.3495
+
+
+def hectagram_to_gram(x):
+    """
+    Function to convert hectagram to gram
+    :param x: Input in hectagram
+    :return: Convert to gram
+    """
+    return x * 100
+
+
+def kilogram_to_gram(x):
+    """
+    Function to convert kilogram to gram
+    :param x: Input in kilogram
+    :return: Convert to gram
+    """
+    return x * 1000
+
+
+def stone_to_gram(x):
+    """
+    Function to convert stone to gram
+    :param x: Input in stone
+    :return: Convert to gram
+    """
+    return x * 6350.29
+
+
+def pound_to_gram(x):
+    """
+    Function to convert pound to gram
+    :param x: Input in pound
+    :return: Convert to gram
+    """
+    return x * 453.592
+
+
+def ton_to_gram(x):
+    """
+    Function to convert ton to gram
+    :param x: Input in ton
+    :return: Convert to gram
+    """
+    return x * kilogram_to_gram(907.185)
