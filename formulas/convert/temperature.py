acd1bd7f790a200ef6cb9b8c17da3533d611025d
pain459 <ravi.ameer@gmail.com>
2024-03-31 17:30:46 +0530

    temperature.py added with conversion math.

---
+++ formulas/convert/temperature.py
@@ -0,0 +1,16 @@
+def celsius_to_fahrenheit(x):
+    """
+    Function to convert Celsius to Fahrenheit
+    :param x: input in Celsius
+    :return: Converted to Fahrenheit
+    """
+    return (x * (9 / 5)) + 32
+
+
+def celsius_to_kelvin(x):
+    """
+    Function to convert Celsius to Kelvin
+    :param x: input in Celsius
+    :return: Converted to Fahrenheit
+    """
+    return x + 273.15
