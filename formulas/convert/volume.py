3b3dda7cc5ee237e82dc1032255bf490e0c1b6a9
pain459 <ravi.ameer@gmail.com>
2024-03-31 18:11:54 +0530

    volume.py updated with formulas.

---
+++ formulas/convert/volume.py
@@ -0,0 +1,101 @@
+import math
+
+
+def milliliter_to_liter(x):
+    """
+    Function to convert milliliter to liter
+    :param x: Input in milliliter
+    :return: converted to liter
+    """
+    return x * 0.001
+
+
+def centiliter_to_liter(x):
+    """
+    Function to convert centiliter to liter
+    :param x: Input in centiliter
+    :return: converted to liter
+    """
+    return x * 0.01
+
+
+def deciliter_to_liter(x):
+    """
+    Function to convert deciliter to liter
+    :param x: Input in deciliter
+    :return: converted to liter
+    """
+    return x * 0.1
+
+
+def decaliter_to_liter(x):
+    """
+    Function to convert decaliter to liter
+    :param x: Input in decaliter
+    :return: converted to liter
+    """
+    return x * 10
+
+
+def hectoliter_to_liter(x):
+    """
+    Function to convert hectoliter to liter
+    :param x: Input in hectoliter
+    :return: converted to liter
+    """
+    return x * 100
+
+
+def kiloliter_to_liter(x):
+    """
+    Function to convert kiloliter to liter
+    :param x: Input in kiloliter
+    :return: converted to liter
+    """
+    return x * 1000
+
+
+def cubic_inch_to_liter(x):
+    """
+    Function to convert cubic inch to liter
+    :param x: Input in cubic inch
+    :return: converted to liter
+    """
+    return 1.639 * math.pow(10, -2)
+
+
+def pint_to_liter(x):
+    """
+    Function to convert pint to liter
+    :param x: Input in pint
+    :return: converted to pint
+    """
+    return x * 0.57
+
+
+def quart_to_liter(x):
+    """
+    Function to convert quart to liter
+    :param x: Input in quart
+    :return: converted to liter
+    """
+    return x * 0.946353
+
+
+def gallon_to_liter(x):
+    """
+    Function to convert gallon-to-liter
+    :param x: Input in gallon
+    :return: converted to liter
+    """
+    return x * 3.785
+
+
+def cubic_foot_to_liter(x):
+    """
+    Function to convert cubic foot-to-liter
+    :param x: Input in cubic foot
+    :return: converted to liter
+    """
+    return x * 28.316
+
